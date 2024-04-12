3ff035f06b378a536cb3cef6ee6a7b0c9737b040
Ibrux11 <ibraluunge@gmail.com>
2024-04-12 18:37:56 +0200

    test

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,3 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
+
+/**
+ mckxytcgvurjkds<
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node);
+{
+
+
+
+
+
+
+
+
+return (0);
+
+return (1);
+
+}
