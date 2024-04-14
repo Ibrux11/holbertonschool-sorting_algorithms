9a998aaf806d8f20d5ed598b4bbc68a8cd0d5ad1
Sofiane74100 <arfane.sofiane@gmail.com>
2024-04-15 00:18:22 +0200

    task1

--- sort.h
+++ sort.h
@@ -27,6 +27,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
-
+void insertion_sort_list(listint_t **list);
 
 #endif
